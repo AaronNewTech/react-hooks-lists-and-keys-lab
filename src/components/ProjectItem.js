c6df00b003c09034eeb29ed254609427eab06147
Aaron <arnhsmith@gmail.com>
2023-07-03 21:17:52 -0400

    code checked and passed all tests

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -2,21 +2,21 @@ import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
   // console.log(technologies)
-  
+  const projectTechnologies = technologies.map((technologies, index) => {
+    return <span key={index}>{technologies}</span>;
+  });
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* {technologies.map(technologies => {
-          {technologies ? <span>{technologies}</span> : null}
-        })} */}
+        {projectTechnologies}
         {/* {technologies.map((technologies) => {
         return {technologies ? <span>{technologies}</span> : null}
         })} */}
-        {technologies[0] ? <span>{technologies[0]}</span> : null}
+        {/* {technologies[0] ? <span>{technologies[0]}</span> : null}
         {technologies[1] ? <span>{technologies[1]}</span> : null}
-        {technologies[2] ? <span>{technologies[2]}</span> : null}
+        {technologies[2] ? <span>{technologies[2]}</span> : null} */}
       </div>
     </div>
   );
