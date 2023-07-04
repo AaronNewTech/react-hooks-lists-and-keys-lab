657c7cf2cfb3381b7927a56ccc06a95aff258b20
Aaron <arnhsmith@gmail.com>
2023-07-03 20:59:46 -0400

    code checked and passed all tests

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,22 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  // console.log(technologies)
+  
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {/* {technologies.map(technologies => {
+          {technologies ? <span>{technologies}</span> : null}
+        })} */}
+        {/* {technologies.map((technologies) => {
+        return {technologies ? <span>{technologies}</span> : null}
+        })} */}
+        {technologies[0] ? <span>{technologies[0]}</span> : null}
+        {technologies[1] ? <span>{technologies[1]}</span> : null}
+        {technologies[2] ? <span>{technologies[2]}</span> : null}
       </div>
     </div>
   );
